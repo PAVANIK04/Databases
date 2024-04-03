b56b663a921778eba5e89c94a13ec17b9fd9d9cc
PAVANIK04 <119106157+PAVANIK04@users.noreply.github.com>
2024-04-03 15:10:45 +0530

    Page name changed to Student, Admin, Faculty. About and LMS Page added to each.

--- library_CS432/config.py
+++ library_CS432/config.py
@@ -1,6 +1,6 @@
 MYSQL_HOST = 'localhost'
 MYSQL_USER = 'root'
-MYSQL_PASSWORD = '1111'
-MYSQL_DB = 'library'
+MYSQL_PASSWORD = 'Pavani04#'
+MYSQL_DB = 'library_final'
 
-SECRET_KEY = '12345'
+SECRET_KEY = '123456789101112131415'

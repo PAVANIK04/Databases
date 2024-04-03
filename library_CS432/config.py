a22e4e0536350664bf477b62682ad3123171ffe3
Anushri231 <143503691+Anushri231@users.noreply.github.com>
2024-04-04 00:07:30 +0530

    push

--- library_CS432/config.py
+++ library_CS432/config.py
@@ -1,6 +1,13 @@
 MYSQL_HOST = 'localhost'
 MYSQL_USER = 'root'
+<<<<<<< Updated upstream
 MYSQL_PASSWORD = '1111'
 MYSQL_DB = 'library'
 
 SECRET_KEY = '12345'
+=======
+MYSQL_PASSWORD = '39272312'
+MYSQL_DB = 'library'
+
+SECRET_KEY = '2804'
+>>>>>>> Stashed changes
